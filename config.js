300fc5e04d4ab29be81270acbf271757baf8667d
GSHugo <vto.garciasanchez98@gmail.com>
2026-08-08 19:25:27 -0600

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.TANDERA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbx1b4wpDSDMz_85recMEVHK2PDJA-3uoT8a_628VPdcH506StubdSPT2ZAIroIXxfODzg/exec",
-  API_KEY: "",
-  SITE_NAME: "Tandera · Revalidación"
-};
