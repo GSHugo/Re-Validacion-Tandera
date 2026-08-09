0e8013723a04c6df50559ad39e4c55848a549853
GSHugo <vto.garciasanchez98@gmail.com>
2026-08-08 19:59:08 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TANDERA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbx1b4wpDSDMz_85recMEVHK2PDJA-3uoT8a_628VPdcH506StubdSPT2ZAIroIXxfODzg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwSvMfXPueocfGOeSYtJSnq7MrFh-9v4LkYafH7OXh8sMePDYwbYnyCiU3aFY_vKzWN5w/exec",
   API_KEY: "",
   SITE_NAME: "Tandera · Revalidación"
 };
